e78ea0ff77ecd9ebbb2798007194003e2db9b1e0
Lilianada <lilibest30.lb@gmail.com>
2023-01-07 07:28:05 -0500

    language switch

--- script.js
+++ script.js
@@ -15,9 +15,10 @@ navLinks.forEach((link) => {
 });
 
 //Onclick for Select language
+// Mobile
 const selectedBox = document.querySelector(".select-container");
-const selected = document.querySelector(".selected-text");
 const selectList = document.querySelector(".select-list");
+const selected = document.querySelector(".selected-text");
 const selectedImg = document.querySelector(".selected-img");
 
 selectedBox.addEventListener("click", () => {
@@ -34,12 +35,24 @@ selectList.addEventListener("click", (e) => {
   e.preventDefault();
 });
 
-// const selectedItem = document.querySelector(".select-wrapper");
-// const selectDropdown = document.querySelector(".select-dropdown");
+// Desktop
+const selectedItem = document.querySelector(".select-wrapper");
+const selectDropdown = document.querySelector(".select-dropdown");
+const imgSelected = document.querySelector("#imgSelected");
+const selectedText = document.querySelector("#langText");
 
-// selectedItem.addEventListener("click", () => {
-//   selectDropdown.classList.toggle("show");
-//   console.log("clicked");
-// });
+selectedItem.addEventListener("click", () => {
+  selectDropdown.classList.toggle("show");
+  console.log("clicked");
+});
+
+selectDropdown.addEventListener("click", (e) => {
+  const selectedText = e.target.querySelector("#langText");
+  const imgSelected = e.target.querySelector("#imgSelected");
+
+  selectedText.innerHTML = lang.innerHTML;
+  imgSelected.src = img.src;
+  e.preventDefault();
+});
 
 //Onclick for games Images
